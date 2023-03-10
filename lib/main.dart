9875d993dfd988a37368a700ab9bbfa5fc5b3757
ASUS\Yiling_Liu <yiling_liu@asus.com>
2023-03-10 11:18:29 +0800

    加上頁面按鈕邏輯

--- lib/main.dart
+++ lib/main.dart
@@ -17,15 +17,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -35,15 +26,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -56,27 +38,30 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
+  void _decrementCounter() {
+    if (_counter == 1) {
+      //由1開始 該數字代表目前顯示的頁面
+      showDialog(
+          context: context,
+          builder: (BuildContext context) => const AlertDialog(
+                title: Text("到達第一頁"),
+                content: Text("您已經在查看第一頁了"),
+              ));
+    } else {
+      setState(() {
+        _counter--;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
@@ -85,26 +70,65 @@ class _MyHomePageState extends State<MyHomePage> {
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
                   return ListView.builder(
-                    itemCount: snapshot.data!.results!.length,
-                    itemBuilder: (context, index) {
-                      return MovieCard(
-                      title: snapshot.data!.results![index].title ?? "",
-                      releaseDate: snapshot.data!.results![index].releaseDate ?? "",
-                      posterUrl: snapshot.data!.results![index].posterPath ?? "",
-                      overview: snapshot.data!.results![index].overview ?? "",
-                      voteAverage: snapshot.data!.results![index].voteAverage ?? 0);
-                  });
+                      itemCount: snapshot.data!.results!.length,
+                      itemBuilder: (context, index) {
+                        return MovieCard(
+                            title: snapshot.data!.results![index].title ?? "",
+                            releaseDate:
+                                snapshot.data!.results![index].releaseDate ??
+                                    "",
+                            posterUrl:
+                                snapshot.data!.results![index].posterPath ?? "",
+                            overview:
+                                snapshot.data!.results![index].overview ?? "",
+                            voteAverage:
+                                snapshot.data!.results![index].voteAverage ??
+                                    0);
+                      });
                 } else if (snapshot.hasError) {
                   return Text('${snapshot.error}');
                 } else {
                   return const CircularProgressIndicator();
                 }
               })),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: _decrementCounter,
+            tooltip: 'Decrement',
+            child: const Icon(Icons.arrow_back_outlined),
+          ),
+          const SizedBox(height: 10), // 加入間距
+          FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: const Icon(Icons.arrow_forward_outlined),
+          ),
+        ],
+      ),
+
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.blue, // 設定背景色為藍色
+        shape: const CircularNotchedRectangle(),
+        child: Container(
+          height: 50.0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Page $_counter",
+                style: Theme.of(context)
+                    .textTheme
+                    .headline6!
+                    .copyWith(color: Colors.white), // 文字顏色設為白色
+              ),
+            ],
+          ),
+        ),
+      ),
+
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
